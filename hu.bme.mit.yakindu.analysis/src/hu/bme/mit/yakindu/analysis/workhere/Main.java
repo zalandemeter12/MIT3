2750d40f1e816539dab2deb852c4566d3ad62359
Demeter Zalán <zalandemeter12@gmail.com>
2022-04-04 14:46:38 +0200

    init

---
+++ hu.bme.mit.yakindu.analysis/src/hu/bme/mit/yakindu/analysis/workhere/Main.java
@@ -0,0 +1,41 @@
+package hu.bme.mit.yakindu.analysis.workhere;
+
+import org.eclipse.emf.common.util.TreeIterator;
+import org.eclipse.emf.ecore.EObject;
+import org.junit.Test;
+import org.yakindu.sct.model.sgraph.State;
+import org.yakindu.sct.model.sgraph.Statechart;
+
+import hu.bme.mit.model2gml.Model2GML;
+import hu.bme.mit.yakindu.analysis.modelmanager.ModelManager;
+
+public class Main {
+	@Test
+	public void test() {
+		main(new String[0]);
+	}
+	
+	public static void main(String[] args) {
+		ModelManager manager = new ModelManager();
+		Model2GML model2gml = new Model2GML();
+		
+		// Loading model
+		EObject root = manager.loadModel("model_input/example.sct");
+		
+		// Reading model
+		Statechart s = (Statechart) root;
+		TreeIterator<EObject> iterator = s.eAllContents();
+		while (iterator.hasNext()) {
+			EObject content = iterator.next();
+			if(content instanceof State) {
+				State state = (State) content;
+				System.out.println(state.getName());
+			}
+		}
+		
+		// Transforming the model into a graph representation
+		String content = model2gml.transform(root);
+		// and saving it
+		manager.saveFile("model_output/graph.gml", content);
+	}
+}
